936e6f905bc0b1556e274c845350d7746a87da0a
Ilya Grigorik <ilya@igvita.com>
2018-10-29 10:18:22 -0700

    fix saas build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,13 @@
+'use strict';
+const sass = require('node-sass');
+
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     sass: {
       options: {
+        implementation: sass,
         includePaths: ['bower_components/foundation/scss']
       },
       dist: {
